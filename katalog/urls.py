85b17652e8fc312a6e1e818c0222c437ee147d2d
Abby Marvel <abby.marvel@ui.ac.id>
2022-09-14 22:08:00 +0700

    update tugas 2

--- katalog/urls.py
+++ katalog/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from katalog.views import show_katalog
 
-app_name = 'tugas2'
+app_name = 'katalog'
 
 urlpatterns = [
     path('', show_katalog, name='show_katalog'),
--- katalog/views.py
+++ katalog/views.py
@@ -4,8 +4,8 @@ from katalog.models import CatalogItem
 def show_katalog(request):
     data_barang_katalog = CatalogItem.objects.all()
     context = {
-        'list_barang': data_barang_katalog,
-        'nama': 'Abby Marvel',
-        'NPM' : '2106751796',
+        'list_barang' : data_barang_katalog,
+        'nama' : 'Abby Marvel',
+        'id' : '2106751796',
     }
     return render(request, "katalog.html", context)
